11fa0f4343149b43453e6fed926f9359399b4434
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-08-21 10:27:29 +0300

    moving code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,16 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+    ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
+
+    setPort(port);
+    
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
@@ -96,15 +106,7 @@ post("/tasks", (request, response) -> {
   return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
 
-ProcessBuilder process = new ProcessBuilder();
- Integer port;
- if (process.environment().get("PORT") != null) {
-     port = Integer.parseInt(process.environment().get("PORT"));
- } else {
-     port = 4567;
- }
 
-setPort(port);
 
   }
 }
