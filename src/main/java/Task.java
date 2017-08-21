3d1f772e5680aaec0c97deef19e9d43b6a678fce
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-08-21 12:59:49 +0300

    all tests passed after creating test database

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,7 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import org.sql2o.*;
 
 public class Task {
   private String description;
@@ -17,31 +18,62 @@ public class Task {
   }
 
   public String getDescription(){
-    return mDescription;
+    return description;
   }
 
   public boolean isCompleted() {
-    return mCompleted;
+    return completed;
   }
 
   public LocalDateTime getCreatedAt() {
-    return mCreatedAt;
+    return createdAt;
   }
 
   public static List<Task> all() {
-   return instances;
+    String sql = "SELECT id, description FROM tasks";
+   try(Connection con = DB.sql2o.open()) {
+     return con.createQuery(sql).executeAndFetch(Task.class);
+   }
+ }
+ //
+ // public static void clear() {
+ //   instances.clear();
+ // }
+
+ @Override
+ public boolean equals(Object otherTask){
+   if (!(otherTask instanceof Task)) {
+     return false;
+   } else {
+     Task newTask = (Task) otherTask;
+     return this.getDescription().equals(newTask.getDescription()) &&
+            this.getId() == newTask.getId();
+   }
  }
 
- public static void clear() {
-   instances.clear();
- }
+public void save() {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "INSERT INTO tasks (description) VALUES (:description)";
+    this.id = (int) con.createQuery(sql, true)
+        .addParameter("description", this.description)
+        .executeUpdate()
+        .getKey();
+  }
+}
+
 
  public int getId() {
-   return mId;
+   return id;
  }
 
  public static Task find(int id) {
-  return instances.get(id - 1);
+   try(Connection con = DB.sql2o.open()) {
+  String sql = "SELECT * FROM tasks where id=:id";
+  Task task = con.createQuery(sql)
+    .addParameter("id", id)
+    .executeAndFetchFirst(Task.class);
+  return task;
+}
 }
 
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,13 +1,14 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.time.LocalDateTime;
+import org.sql2o.*;
 
 public class TaskTest {
 
 //connecting to test database
   @Before
 public void setUp() {
-  DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
+  DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", "gnjoroge", "1234");
 }
 
 //clearing the test database
@@ -43,35 +44,66 @@ public void getCreatedAt_instantiatesWithCurrentTime_today() {
   assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
 }
 
+
 @Test
 public void all_returnsAllInstancesOfTask_true() {
   Task firstTask = new Task("Mow the lawn");
+  firstTask.save();
   Task secondTask = new Task("Buy groceries");
-  assertEquals(true, Task.all().contains(firstTask));
-  assertEquals(true, Task.all().contains(secondTask));
+  secondTask.save();
+  assertEquals(true, Task.all().get(0).equals(firstTask));
+  assertEquals(true, Task.all().get(1).equals(secondTask));
 }
 
-@Test
-public void clear_emptiesAllTasksFromArrayList_0() {
-  Task myTask = new Task("Mow the lawn");
-  //Task.clear();
-  assertEquals(Task.all().size(), 0);
-}
+// @Test
+// public void clear_emptiesAllTasksFromArrayList_0() {
+//   Task myTask = new Task("Mow the lawn");
+//   //Task.clear();
+//   assertEquals(Task.all().size(), 0);
+// }
 
 //assign and access IDs
 @Test
-public void getId_tasksInstantiateWithAnID_1() {
+public void getId_tasksInstantiateWithAnID() {
 //  Task.clear();  // Remember, the test will fail without this line! We need to empty leftover Tasks from previous tests!
   Task myTask = new Task("Mow the lawn");
-  assertEquals(1, myTask.getId());
+  myTask.save();
+  assertTrue(myTask.getId() > 0);
 }
 
 //locating specific Tasks using their unique ID
 @Test
 public void find_returnsTaskWithSameId_secondTask() {
   Task firstTask = new Task("Mow the lawn");
+  firstTask.save();
   Task secondTask = new Task("Buy groceries");
+  secondTask.save();
   assertEquals(Task.find(secondTask.getId()), secondTask);
+}
+
+//can recognize similar objects
+@Test
+public void equals_returnsTrueIfDescriptionsAreTheSame(){
+   Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Mow the lawn");
+    assertTrue(firstTask.equals(secondTask));
+}
+
+//method to save objects into our database
+@Test
+public void save_returnsTrueIfDescriptionsAretheSame() {
+  Task myTask = new Task("Mow the lawn");
+  myTask.save();
+  assertTrue(Task.all().get(0).equals(myTask));
+}
+
+//assigning unique IDs
+@Test
+public void save_assignsIdToObject() {
+  Task myTask = new Task("Mow the lawn");
+  myTask.save();
+  Task savedTask = Task.all().get(0);
+  assertEquals(myTask.getId(), savedTask.getId());
 }
 
 }
